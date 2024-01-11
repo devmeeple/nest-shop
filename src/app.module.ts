a8233c87c30f1b6439db2993d70a3fb9f719fd05
TaeGeun Jang <devmeeple@gmail.com>
2024-01-11 22:30:31 +0900

    feat: 데이터베이스 환경설정 (#1)
    - docker-compose 작성
    - config module 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from './common/common.module';
+import { TypeormConfigService } from './common/config/typeorm.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeormConfigService,
+    }),
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
