7db46c2b9d9bcc2f45c710b84a897ccc9dc55843
TaeGeun Jang <devmeeple@gmail.com>
2024-01-11 22:30:31 +0900

    feat: 데이터베이스 환경설정 (#1)
    - docker-compose 작성
    - config module 사용

---
+++ src/common/config/typeorm.config.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class TypeormConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(
+    connectionName?: string,
+  ): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DATABASE_HOST'),
+      port: this.configService.get<number>('DATABASE_PORT'),
+      username: this.configService.get<string>('DATABASE_USERNAME'),
+      password: this.configService.get<string>('DATABASE_PASSWORD'),
+      database: this.configService.get<string>('DATABASE_NAME'),
+      entities: [],
+      synchronize: true,
+      logging: true,
+    };
+  }
+}
