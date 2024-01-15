de8c89dc6f0f2d0b40a4ca873c792bf061810a6b
미플 <devmeeple@gmail.com>
2024-01-15 22:54:23 +0900

    feat: items 모듈 구현
    - 엔티티 개발(비즈니스 로직 추가)
    - 상품 서비스 개발

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -7,6 +7,7 @@ import {
   TableInheritance,
 } from 'typeorm';
 import { Category } from './category.entity';
+import { NotEnoughStockException } from '../../common/exception/not-enough-stock.exception';
 
 @Entity()
 @TableInheritance({ column: { type: 'varchar', name: 'type' } })
@@ -26,4 +27,24 @@ export class Item {
   @ManyToMany(() => Category)
   @JoinTable()
   categories: Category[];
+
+  /**
+   * 비즈니스 로직
+   * 데이터를 가지고 있는 엔티티에 비즈니스 로직을 추가하여 응집력을 높인다
+   * 재고 증가
+   */
+  addStock(quantity: number) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * 재고 감소
+   */
+  removeStock(quantity: number) {
+    const restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+      throw new NotEnoughStockException('추가재고가 필요합니다');
+    }
+    this.stockQuantity = restStock;
+  }
 }
