90441627c2008f7ec87d464e0bbe2cf8bc75b217
TaeGeun Jang <devmeeple@gmail.com>
2024-01-15 23:02:29 +0900

    feat: items 모듈 구현 (#5)
    - 엔티티 개발(비즈니스 로직 추가)
    - 상품 서비스 개발

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item) private readonly itemRepository: Repository<Item>,
+  ) {}
+
+  /**
+   * 이미 값이 존재하면 UPDATE 없으면 INSERT
+   */
+  async saveItem(item: Item) {
+    return await this.itemRepository.save(item);
+  }
+
+  async findItems() {
+    return await this.itemRepository.find();
+  }
+
+  async findOne(itemId: number) {
+    return await this.itemRepository.findOne({ where: { id: itemId } });
+  }
+}
